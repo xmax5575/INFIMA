a61a87788606c8053649a4726da63e884bb8ccd3
marijadundovic <md55868@fer.hr>
2025-11-04 14:58:58 +0100

    Fix some errors
    Fixed some remaining errors.

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.jsx";
 import "../src/styles/index.css";
-
-// Get this from your Google Cloud Console
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
