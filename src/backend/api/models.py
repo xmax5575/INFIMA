785a136a7955f2050878b781d4cd44f206bb60f9
karlokrstic <kk56110@fer.hr>
2025-11-06 10:03:59 +0100

    Update models.py

--- src/backend/api/models.py
+++ src/backend/api/models.py
@@ -19,3 +19,16 @@ class User(AbstractUser):
     @property
     def has_role(self):
         return self.role is not None
+
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+if created:
+    Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
