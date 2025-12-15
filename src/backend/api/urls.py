d3c95aeb01f683645c34f5b96520cecf8bd3c7d5
karlokrstic <kk56110@fer.hr>
2025-12-15 14:56:43 +0100

    Url.py syntax fix

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Endpoint za dohvat, ažuriranje ili brisanje pojedine lekcije prema ID-u (GET, PUT/PATCH, DELETE)
     path('lessons/<int:pk>/', views.LessonDetailView.as_view(), name='lesson-detail'),
     # Endpoint preko kojeg će instruktor uređivati svoj profil
-    path('instructor/me/', views.InstructorUpdateView.as_view(), name='instructor-update')
+    path('instructor/me/', views.InstructorUpdateView.as_view(), name='instructor-update'),
     path("instructor/inf/", views.MyInstructorProfileView.as_view(), name="instructor-inf"),
     path("instructor/<int:pk>/", views.InstructorPublicProfileView.as_view(), name="instructor-detail"),
 ]
