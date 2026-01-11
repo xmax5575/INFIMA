78da3836e14df49de2088666f85a4b55ca50f0e1
Max Matisic <max.matisic@fer.hr>
2026-01-11 22:34:38 +0100

    Hard sync migrations folder from main

---
+++ src/backend/api/migrations/0007_remove_instructor_subject_instructor_sujects_and_more.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0006_alter_lesson_instructor_and_more'),
+    ]
+
+    operations = [
+    ]
---
+++ src/backend/api/migrations/0013_rename_sujects_instructor_subjects.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0012_remove_instructor_sujects_lesson_status_and_more'),
+    ]
+
+    operations = [
+    ]
