afb7b77de04d3fcf8ef50fc11d8f990b8093e34c
Max Matisic <max.matisic@fer.hr>
2026-01-13 02:47:56 +0100

    Update TerminCard.jsx
    Updated some buttons on TerminCard.jsx.

--- src/frontend/src/components/TerminCard.jsx
+++ src/frontend/src/components/TerminCard.jsx
@@ -177,7 +177,7 @@ export default function TerminCard({
             {reserved && !expired && (
               <button
                 onClick={() => onReserveOrCancel(lesson_id)}
-                className="px-4 py-2 bg-[#DC2626] text-white rounded-xl hover:bg-[#B91C1C] hover:scale-105 duration-[500ms] ease-in-out ring-1"
+                className="px-4 py-2 bg-[#DC2626] text-white rounded-xl hover:scale-105 duration-[500ms] ease-in-out ring-1"
               >
                 Otkaži
               </button>
@@ -186,7 +186,7 @@ export default function TerminCard({
             {canReserve && !reserved && !expired && (
               <button
                 onClick={() => onReserveOrCancel(lesson_id)}
-                className="px-4 py-2 bg-[#3674B5] text-white rounded-xl hover:bg-[#1E3A8A] hover:scale-105 duration-[500ms] ease-in-out ring-1"
+                className="px-4 py-2 bg-[#3674B5] text-white rounded-xl hover:scale-105 duration-[500ms] ease-in-out ring-1"
               >
                 Rezerviraj
               </button>
@@ -207,7 +207,7 @@ export default function TerminCard({
         {role === "instructor" && format === "Online" && (
           <button
             onClick={goToMeeting}
-            className="px-4 py-2 bg-green-700 text-white rounded-xl mt-4"
+            className="px-4 py-2 bg-green-600 text-white rounded-xl hover:scale-105 duration-[500ms] ease-in-out ring-1"
           >
             Pokreni meeting
           </button>
